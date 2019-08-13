f937e4344d440e72ef76e349e635e330ce5077f7
Vux <alextuanit@gmail.com>
2019-08-13 18:38:51 +0700

    ..

--- routes/update.db.js
+++ routes/update.db.js
@@ -17,42 +17,12 @@ let updateMongo = async(listDomain) => {
             let data = await Url.findOne({ _id: item.id });
             let arrTimeLoad = [];
             arrTimeLoad = data.timeLoad;
+
             console.log(arrTimeLoad, item.url)
             if (data.timeLoad.length < 10) {
                 console.log('else <10')
                 arrTimeLoad.push(item.timeLoad);
-                let count = 0;
-                console.log('fdfddfdffdffdfdfddffddfdfdfdfdf')
-                let checkWebDeath = async() => {
-                    for (let i = 0; i < arrTimeLoad.length(); i++) {
-                        console.log('gggggg')
-                        if (arr[i] == 0) {
-                            count++;
-                            console.log('gggggggggggggggggg')
-                            if (count == 3) {
-                                let urlData = item.url + ' Has a trouble!';
-                                console.log(urlData, typeof urlData)
-                                let object = {
-                                    "msgtype": "text",
-                                    "text": {
-                                        "content": urlData
-                                    }
-                                }
-                                console.log('ggggggggggggggggggggggggggg')
-                                axios.post('https://oapi.dingtalk.com/robot/send?access_token=f54b31959e65a16de90c41b3640cbc499824b45832ab57bed0249af360247b71', {...object })
-                                    .then(data22 => {
-                                        console.log(data22.data);
-                                    }).catch(err => {
-                                        console.log(err)
-                                    });
-                                break;
-                            }
-                        } else {
-                            count = 0;
-                        }
-                    }
-                }
-                await checkWebDeath()
+
                 try {
                     let update = await Url.updateOne({ _id: item.id }, {
                         $set: {
@@ -80,6 +50,39 @@ let updateMongo = async(listDomain) => {
 }
 
 module.exports = updateMongo;
+
+// let count = 0;
+//                 console.log('fdfddfdffdffdfdfddffddfdfdfdfdf')
+//                 let checkWebDeath = async() => {
+//                     for (let i = 0; i < arrTimeLoad.length(); i++) {
+//                         console.log('gggggg')
+//                         if (arr[i] == 0) {
+//                             count++;
+//                             console.log('gggggggggggggggggg')
+//                             if (count == 3) {
+//                                 let urlData = item.url + ' Has a trouble!';
+//                                 console.log(urlData, typeof urlData)
+//                                 let object = {
+//                                     "msgtype": "text",
+//                                     "text": {
+//                                         "content": urlData
+//                                     }
+//                                 }
+//                                 console.log('ggggggggggggggggggggggggggg')
+//                                 axios.post('https://oapi.dingtalk.com/robot/send?access_token=f54b31959e65a16de90c41b3640cbc499824b45832ab57bed0249af360247b71', {...object })
+//                                     .then(data22 => {
+//                                         console.log(data22.data);
+//                                     }).catch(err => {
+//                                         console.log(err)
+//                                     });
+//                                 break;
+//                             }
+//                         } else {
+//                             count = 0;
+//                         }
+//                     }
+//                 }
+//                 await checkWebDeath()
 
 // url: 'simsaigon.com.vn',
 //   domain: 'https://simsaigon.com.vn',
