5a3110ad6b7362fe02cba264951ee26912a53e63
Vux <alextuanit@gmail.com>
2019-08-14 18:29:27 +0700

    ,,

--- controller/employee.js
+++ controller/employee.js
@@ -1,25 +1,34 @@
 const cheerio = require('cheerio');
 const axios = require('axios');
+var request = require("request");
+var https = require('https');
 
 module.exports = {
     employeeCheckOn: async(req, res) => {
-        // let header = {
-        //     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36',
-        //     'Content-Type': 'text/html; charset=UTF-8'
-        // }
-        // let options = {
-        //     url: 'https://118.70.81.234:8443/api/thongketongdai.php',
-        //     method: 'GET',
-        //     headers: header
-        // }
-        // let data = await axios(options);
-        // if (data) {
-        //     let $ = cheerio.load(data);
-        //     console.log($('th').html())
-        // }
-        res.render('employeeCheck.ejs', {
-            title: 'employee'
-        })
+        let url = "https://118.70.81.234:8443/api/thongketongdai.php"
+        const fruits = [];
+        let data = async() => {
+            let arr = await axios.get("https://118.70.81.234:8443/api/thongketongdai.php", {
 
+                httpsAgent: new https.Agent({
+                    rejectUnauthorized: false
+                })
+            });
+            if (arr) {
+                var $ = cheerio.load(arr.data);
+                $("tr:not(:nth-of-type(2)):not(:nth-of-type(1))").each(function() {
+                    $(this).find("th[style]").each(function() {
+                        let textval = $(this).text()
+                        fruits.push(textval.trim())
+                    })
+                })
+                return fruits;
+            }
+        }
+        let i = async() => {
+            let arr1 = await data();
+            console.log(arr1)
+        }
+        i()
     }
 }
