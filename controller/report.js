8fc7104ed036f55966a90e31577d1ff521482498
master vux <dangquangvu999@gmail.com>
2019-11-19 09:57:56 +0700

    xx

--- controller/report.js
+++ controller/report.js
@@ -1,19 +1,19 @@
-const moment = require('moment')
-var { Url, User, TimeCheck } = require('../db/index')
-const axios = require('axios');
+const moment = require("moment");
+var { Url, User, TimeCheck } = require("../db/index");
+const axios = require("axios");
 module.exports = {
     reportTime: async(req, res) => {
         let startDate = req.body.startDate;
-        console.log(startDate)
+        console.log(startDate);
         let endDate = req.body.endDate;
-        console.log(endDate)
+        console.log(endDate);
         let urlUser = req.body.selectUrl;
-        let iduser = '5d4bdc714f7d0f77e5a9492b';
+        let iduser = "5d4bdc714f7d0f77e5a9492b";
         const arrData = [];
         if (startDate) {
             let findDate = async() => {
                 let date = moment(startDate).format("YYYY-MM-DDTHH:mm:ss.SSSZ");
-                console.log(date)
+                console.log(date);
                 let data = await TimeCheck.find({
                     idUser: iduser,
                     getUrl: urlUser,
@@ -24,20 +24,21 @@ module.exports = {
                 if (data) {
                     await data.map(async item => {
                         let datetime = item.dateTime;
-                        let utcDate = moment(datetime).utcOffset("+07:00").format("YYYY-MM-DDTHH:mm:ss.SSSZ");
+                        let utcDate = moment(datetime)
+                            .utcOffset("+07:00")
+                            .format("YYYY-MM-DDTHH:mm:ss.SSSZ");
                         var myDate = new Date(utcDate);
                         var result = myDate.getTime();
                         let resultDate = result;
                         let timeload = item.timeLoad;
-                        let objectdata = [resultDate, timeload]
+                        let objectdata = [resultDate, timeload];
                         arrData.push(objectdata);
-                    })
-                    res.send(arrData)
-
+                    });
+                    res.send(arrData);
                 } else {
-                    console.log('err-get-reports')
+                    console.log("err-get-reports");
                 }
-            }
+            };
             await findDate();
         } else if (endDate) {
             let findDate = async() => {
@@ -50,22 +51,23 @@ module.exports = {
                     }
                 });
                 if (data) {
-                    await data.map(async item => {
+                    data.map(item => {
                         let datetime = item.dateTime;
-                        let utcDate = moment(datetime).utcOffset("+07:00").format("YYYY-MM-DDTHH:mm:ss.SSSZ");
+                        let utcDate = moment(datetime)
+                            .utcOffset("+07:00")
+                            .format("YYYY-MM-DDTHH:mm:ss.SSSZ");
                         var myDate = new Date(utcDate);
                         var result = myDate.getTime();
                         let resultDate = result;
                         let timeload = item.timeLoad;
-                        let objectdata = [resultDate, timeload]
+                        let objectdata = [resultDate, timeload];
                         arrData.push(objectdata);
-                    })
-                    res.json(arrData)
-
+                    });
+                    res.json(arrData);
                 } else {
-                    console.log('err-get-reports')
+                    console.log("err-get-reports");
                 }
-            }
+            };
             await findDate();
         } else if (endDate && startDate) {
             let findDate = async() => {
@@ -78,72 +80,76 @@ module.exports = {
                         $gte: sdate,
                         $lte: edate
                     }
-                })
+                });
                 if (data) {
-                    await data.map(async item => {
-                            let datetime = item.dateTime;
-                            let utcDate = moment(datetime).utcOffset("+07:00").format("YYYY-MM-DDTHH:mm:ss.SSSZ");
-                            var myDate = new Date(utcDate);
-                            var result = myDate.getTime();
-                            let resultDate = result;
-                            let timeload = item.timeLoad;
-                            let objectdata = [resultDate, timeload]
-                            arrData.push(objectdata);
-                        })
-                        // .map(item => {
-                        //     console.log(item);
-                        // })
-                    res.json(arrData)
-
+                    data.map(item => {
+                        let datetime = item.dateTime;
+                        let utcDate = moment(datetime)
+                            .utcOffset("+07:00")
+                            .format("YYYY-MM-DDTHH:mm:ss.SSSZ");
+                        var myDate = new Date(utcDate);
+                        var result = myDate.getTime();
+                        let resultDate = result;
+                        let timeload = item.timeLoad;
+                        let objectdata = [resultDate, timeload];
+                        arrData.push(objectdata);
+                    });
+                    // .map(item => {
+                    //     console.log(item);
+                    // })
+                    res.json(arrData);
                 } else {
-                    console.log('err-get-reports')
+                    console.log("err-get-reports");
                 }
-            }
+            };
             await findDate();
         }
     },
     showReports: async function(req, res) {
-        let iduser = '5d4bdc714f7d0f77e5a9492b';
+        let iduser = "5d4bdc714f7d0f77e5a9492b";
         const arrDataUrlReporst = [];
         let data_iduser_url = async() => {
             try {
-                let data = await Url.find({ idUser: iduser, hidden: false })
+                let data = await Url.find({ idUser: iduser, hidden: false });
                 if (data) {
                     data.map(item => {
                         arrDataUrlReporst.push(item.getUrl);
-                    })
+                    });
                 }
                 return arrDataUrlReporst;
             } catch (error) {
-                console.log('err-get-reports')
+                console.log("err-get-reports");
             }
-        }
+        };
         let dataHanlerUrlReports = await data_iduser_url();
-        res.render('time-report.ejs', { title: 'report', data: dataHanlerUrlReports });
+        res.render("time-report.ejs", {
+            title: "report",
+            data: dataHanlerUrlReports
+        });
     },
     dataReports: async(req, res, next) => {
-        let b = req.body.arrData;
-        res.json(b)
-    },
-    // reportMess: async() => {
-    //     let count = 0;
-    //     console.log(arrTimeLoad, item.id)
-    //     let checkWebDeath = async(arrTimeLoad) => {
-    //         for (let i = 0; i < arrTimeLoad.length(); i++) {
-    //             console.log(arrTimeLoad, 333333333333333333)
-    //             if (arr[i] == 0) {
-    //                 count++;
-    //                 console.log(count)
-    //                 if (count == 3) {
-    //                     let urlData = item.url + ' Has a trouble!';
-    //                     console.log(urlData, typeof item.url)
-    //                     let object = {
-    //                         "msgtype": "text",
-    //                         "text": {
-    //                             "content": urlData
-    //                         }
-    //                     }
-    //                     console.log('ggggggggggggggggggggggggggg')
+            let b = req.body.arrData;
+            res.json(b);
+        }
+        // reportMess: async() => {
+        //     let count = 0;
+        //     console.log(arrTimeLoad, item.id)
+        //     let checkWebDeath = async(arrTimeLoad) => {
+        //         for (let i = 0; i < arrTimeLoad.length(); i++) {
+        //             console.log(arrTimeLoad, 333333333333333333)
+        //             if (arr[i] == 0) {
+        //                 count++;
+        //                 console.log(count)
+        //                 if (count == 3) {
+        //                     let urlData = item.url + ' Has a trouble!';
+        //                     console.log(urlData, typeof item.url)
+        //                     let object = {
+        //                         "msgtype": "text",
+        //                         "text": {
+        //                             "content": urlData
+        //                         }
+        //                     }
+        //                     console.log('ggggggggggggggggggggggggggg')
 
     //                     axios.post('https://oapi.dingtalk.com/robot/send?access_token=da3bfd30996829aad8dfdbccdc208148b612f95b7ac3fe457661cbcfed3de037', {...object })
     //                         .then(data22 => {
@@ -160,6 +166,6 @@ module.exports = {
     //     }
     //     await checkWebDeath(arrTimeLoad)
     // }
-}
+};
 
 // moment(endDate).utcOffset('+0700').format("YYYY-MM-DDTHH:mm:ss.SSSZ");
--- routes/getDomain.js
+++ routes/getDomain.js
@@ -1,62 +1,56 @@
-var { Url } = require('../db/index')
+var { Url } = require("../db/index");
 arrData = [];
-var getDomain = async(arrData) => {
-        // const iduser = '5d380cabf99f7858fc33e9c4'
-        //console.log('xxx')
-        const arrDataMongo = []
-            //{ $and: [{ flag: true }, { idUser: iduser }]}
-        let data = await Url.find({ flag: true, hidden: false }).limit(10);
-        console.log(data.length, typeof data)
-        let mapItem = await data.map(item => {
-            arrDataMongo.push(item);
-        })
-        if (arrDataMongo.length > 5) {
-            for (let i = 0; i < 5; i++) {
-                //console.log('xxx')
-                let url = arrDataMongo[i].getUrl;
-                let domain = arrDataMongo[i].protocolUrl;
-                let id = arrDataMongo[i]._id;
-                let iduser = arrDataMongo[i].idUser;
-                let timeLoad = 0;
-                let date = 0;
-                let object = { url, domain, date, timeLoad, id, iduser };
-                arrData.push(object);
-                try {
-                    let data = await Url.updateOne({ _id: id }, {
-                        $set: {
-                            flag: false
-                        }
-                    })
-                } catch (err) {
-
-                }
-            }
-            console.log('if')
-        } else if (arrDataMongo.length <= 5) {
-            await arrDataMongo.map(async item => {
-                console.log('yyy')
-                let url = item.getUrl;
-                let domain = item.protocolUrl;
-                let id = item._id;
-                let iduser = item.idUser;
-                let timeLoad = 0;
-                let date = 0;
-                let object = { url, domain, date, timeLoad, id, iduser };
-                //console.log(object, typeof arrData)
-                await arrData.push(object);
-            })
+var getDomain = async arrData => {
+    // const iduser = '5d380cabf99f7858fc33e9c4'
+    //console.log('xxx')
+    const arrDataMongo = [];
+    //{ $and: [{ flag: true }, { idUser: iduser }]}
+    let data = await Url.find({ flag: true, hidden: false }).limit(10);
+    console.log(data.length, typeof data);
+    let mapItem = data.map(item => {
+        arrDataMongo.push(item);
+    });
+    if (arrDataMongo.length > 5) {
+        for (let i = 0; i < 5; i++) {
+            //console.log('xxx')
+            let url = arrDataMongo[i].getUrl;
+            let domain = arrDataMongo[i].protocolUrl;
+            let id = arrDataMongo[i]._id;
+            let iduser = arrDataMongo[i].idUser;
+            let timeLoad = 0;
+            let date = 0;
+            let object = { url, domain, date, timeLoad, id, iduser };
+            arrData.push(object);
             try {
-                let data = await Url.updateMany({}, {
+                let data = await Url.updateOne({ _id: id }, {
                     $set: {
-                        flag: true
+                        flag: false
                     }
-                }, { multi: true })
-            } catch (err) {
-
-            }
-            console.log('else');
+                });
+            } catch (err) {}
         }
-        return arrData;
+        console.log("if");
+    } else if (arrDataMongo.length <= 5) {
+        arrDataMongo.map(item => {
+            let url = item.getUrl;
+            let domain = item.protocolUrl;
+            let id = item._id;
+            let iduser = item.idUser;
+            let timeLoad = 0;
+            let date = 0;
+            let object = { url, domain, date, timeLoad, id, iduser };
+            arrData.push(object);
+        });
+        try {
+            let data = await Url.updateMany({}, {
+                $set: {
+                    flag: true
+                }
+            }, { multi: true });
+        } catch (err) {}
+        console.log("else");
     }
-    //getDomain();
+    return arrData;
+};
+//getDomain();
 module.exports = getDomain;
